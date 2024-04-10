ca8f4f5305796bd46f5eb6653fac5466c05dc460
Charuhas Reddy <charuhasreddybalam@gmail.com>
2024-04-10 12:04:57 +0530

    added more routes

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -9,13 +9,13 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import { CgProfile } from "react-icons/cg";
-import { FaWallet, FaBookOpen } from "react-icons/fa";
+import { FaWallet, FaBookOpen, FaBook } from "react-icons/fa";
 import { IoInformationCircle } from "react-icons/io5";
 import { LuHistory } from "react-icons/lu";
 
 import Link from "next/link";
 
-const drawerWidth = 70; // Adjusted to make the drawer minimized
+const drawerWidth = 70;
 
 const Drawer = styled(MuiDrawer)(({ theme }) => ({
   width: drawerWidth,
@@ -33,16 +33,31 @@ const Drawer = styled(MuiDrawer)(({ theme }) => ({
 
 const iconList = [
   <CgProfile />,
-  <FaWallet />,
+  <FaBook />,
   <FaBookOpen />,
+  <FaWallet />,
   <LuHistory />,
   <IoInformationCircle />,
 ];
 
 export default function MiniDrawer() {
-  const links = ["/profile", "/wallet", "/mycourses","/transaction", "/about"];
+  const links = [
+    "/profile",
+    "/courses",
+    "/mycourses",
+    "/wallet",
+    "/transaction",
+    "/about",
+  ];
   const theme = useTheme();
-  const titles = ["profile", "wallet", "mycourses","transaction", "about"]; // Titles corresponding to the links
+  const titles = [
+    "Profile",
+    "Courses",
+    "My Courses",
+    "Wallet",
+    "Transaction",
+    "About",
+  ];
 
   return (
     <Box sx={{ display: "flex" }}>
